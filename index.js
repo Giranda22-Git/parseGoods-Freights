1f67e661182b2ac7ea27af09a178bea267d1cc6f
Giranda22-Git <aloxaeee@bk.ru>
2022-02-08 17:14:44 +0600

    init

---
+++ index.js
@@ -0,0 +1,63 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+// require and connect telegram bot
+//const bot = require('./botCommands/connection.js')
+
+// require bot middleware
+//----
+
+// enable bot middleware
+//----
+
+// require mounted data as serverData
+const serverData = require('./staticData/mountedData.js').data
+
+// activate mounted functions
+//----
+
+// require simple functions
+//----
+
+// initial express js application
+const app = express()
+
+// standart middleweare settings
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use((req, res, next) => {
+	res.contentType('application/json')
+	next()
+})
+app.use(cors())
+
+
+// function of init all process
+async function init(serverData) {
+	await mongoose.connect(serverData.mongoUrl, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true
+	})
+
+  mongoose.connection.once('open', () => {
+		app.listen(serverData.PORT, '0.0.0.0', (err) => {
+			if (err) return new Error(`error in starting server, error: ${err}`)
+			else console.log(`server started on \nPORT: ${serverData.PORT}\nURL: ${serverData.serverUrl}`)
+		})
+
+    // require all end points
+		//app.use('/webhook', require('./endPoints/webhook.js'))
+	})
+
+  // require bot commands
+  //require('./botCommands/addToBasketAction.js')
+
+  //await centralDataBaseParse()
+
+	mongoose.connection.emit('open')
+  //bot.launch()
+}
+
+init (serverData)
---
+++ staticData/mountedData.js
@@ -0,0 +1,14 @@
+const data = {
+  mongoUrl: 'mongodb://localhost:27017/parseGoods-Freights',
+	serverUrl: 'http://localhost:3000/',
+  interiorServerUrl: 'void',
+  telegramBotToken: '2026846097:AAGkyUdk5ZHGBQtq0nrQKdfEJ9ApxA3EoX8',
+  standartLanguageCode: 'ru',
+  retailCrmUrl: 'https://cloudvape.retailcrm.ru/api/v5/',
+  retailCrmApiKey: '?apiKey=NUs8xtV0sowUdPt3efhCldls2NhsaCxK',
+  adminChatId: -575693357,
+  logsChatId: -758687223,
+	PORT: 3000
+}
+
+module.exports = {data}
